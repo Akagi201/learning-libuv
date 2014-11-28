3f75a8b11fe8768b10ccdbade4a3fa71f3a1c258
Akagi201 <akagi201@gmail.com>
2014-11-28 23:28:26 +0800

    update detach

--- detach/main.c
+++ detach/main.c
@@ -9,7 +9,7 @@ uv_process_options_t options;
 int main() {
     loop = uv_default_loop();
 
-    char* args[3];
+    char *args[3];
     args[0] = "sleep";
     args[1] = "100";
     args[2] = NULL;
@@ -25,7 +25,7 @@ int main() {
         return 1;
     }
     fprintf(stderr, "Launched sleep with PID %d\n", child_req.pid);
-    uv_unref((uv_handle_t*) &child_req);
+    uv_unref((uv_handle_t *) &child_req);
 
     return uv_run(loop, UV_RUN_DEFAULT);
 }
